d1201fd457cb6d777f6f597b4c9ebf045c75c3ea
Andrew Iskandarov <ronyn@list.ru>
2020-10-27 10:23:15 +0300

    27102020 commit #1

---
+++ tableTest/Models/UserData.swift
@@ -0,0 +1,13 @@
+//
+//  UserData.swift
+//  tableTest
+//
+//  Created by Andrew Iskandarov on 26.10.2020.
+//import Foundation
+
+struct UserData: Codable {
+   let userId: Int?
+   let id: Int?
+   let title: String?
+   let completed: Bool?
+}
